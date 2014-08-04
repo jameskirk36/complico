b5d469d55bb718f24172d42a4a9334c40c1df6cf
James Kirk <james.kirk84@gmail.com>
2014-08-04 20:56:33 +0100

    extracting host url

--- src/complico/core.clj
+++ src/complico/core.clj
@@ -2,12 +2,20 @@
   (use compojure.core)
   (use compojure.route)
   (use ring.middleware.params)
+  (use [clojure.string :only [join split]])
 )
+
+; extract just the host from a URI
+(defn extract-host [url]
+  (join "" 
+    [(join "/" 
+      (take 3 
+        (split url #"/"))) "/"])) 
 
 (defroutes my-handler
   (GET "/" [] "Welcome")
   (GET "/convert" {params :query-params} 
-    (str "<head><base href=\"" (params "url")  \""/></head>")))
+    (str "<head><base href=\"" (extract-host (params "url"))  \""/></head>")))
 
 (def app
   (wrap-params my-handler))
--- test/complico/acceptance_tests.clj
+++ test/complico/acceptance_tests.clj
@@ -4,10 +4,11 @@
 		[ring.mock.request]
 		[complico.core]))
 
-(deftest convert-should-add-base-element
-  (testing "convert should add base element"
+(deftest convert
+  (testing "convert should add url as base element"
     (let [response (app (request :get "/convert" {:url "http://www.host.com/page.html"} ))]
       (is (= (:status response) 200))
-      (is (= (.contains (:body response) "<head><base href=\"http://www.host.com/page.html\"/></head>") true))))
+		(println (:body response))
+      (is (= (.contains (:body response) "<head><base href=\"http://www.host.com/\"/></head>") true))))
 )
 
