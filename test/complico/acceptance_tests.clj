9c99c5cf341afd3ddc51467a66858d81fc8a19db
James Kirk <james.kirk84@gmail.com>
2014-08-05 07:35:14 +0100

    first test actually passing now. ahem

--- test/complico/acceptance_tests.clj
+++ test/complico/acceptance_tests.clj
@@ -6,13 +6,15 @@
       [ring.util.serve]))
 
 (deftest convert
-  (testing "convert should add url as base element"
-    (serve my-handler)
+  (testing "should add host url as base element to request body"
+
+    ; start the server, headless
+    (serve-headless my-handler)
+
     (let [response (app (request :get "/convert" {:url "http://localhost:3000/test"} ))]
       (is (= (:status response) 200))
 		;(println (:body response))
       (is (= (.contains (:body response) 
 			"<head><base href=\"http://localhost:3000/\"/></head>Test Page") true)))
-    (stop-server))
-)
+    (stop-server)))
 
