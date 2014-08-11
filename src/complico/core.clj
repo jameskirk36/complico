1354f712b6c2f9330cb3ab451154fa7c52cc178b
James Kirk <james.kirk84@gmail.com>
2014-08-11 17:22:44 +0100

    refactoring and formatting

--- src/complico/core.clj
+++ src/complico/core.clj
@@ -21,38 +21,48 @@
 (defn read-request-body [url] 
   (:body (client/get url)))
 
+; find a sequence of anchor links in a given body
 (defn find-links [body]
   (re-seq #"a href=\"(.+?)\"" body))
 
+; create the prefix to add to all anchor links
 (defn create-grease [server port]
   (str "a href=\"http://" server ":" port "/convert?url="))
 
+; apply grease to different kinds of links
 (defn grease-the-link [link grease host]
   (cond
     (.startsWith link "http") (str grease link "\"")
     (.startsWith link "/") (str grease (apply str (drop-last 1 host)) link "\"") 
     :else (str grease host link "\""))) 
 
+; replace all the links in the body with their greased equivalents
 (defn replace-links [body links grease host]
   (str
-  (reduce 
-    #(apply string/replace %1 %2)
-    body
-    (map 
-      #(vector 
-        (first %)
-        (grease-the-link (last %) grease host)) links)
+    (reduce 
+      #(apply string/replace %1 %2)
+      body
+      (map 
+        #(vector 
+          (first %)
+          (grease-the-link 
+            (last %)
+            grease
+            host))
+       links)
      )))
 
+; 
 (defn grease-the-links [body server port host]
   (let [links (find-links body)]
     (replace-links body links (create-grease server port) host)))
 
 (defroutes my-handler
   (GET "/" [] "Welcome")
-  ; route for testing only
+  ; routes for testing only
   (GET "/test_full_link" [] "Test Page<a href=\"http://somelink.com/\">")
   (GET "/test_relative_link" [] "Test Page<a href=\"/duff\">")
+  ; entry point for complicos complicated prices!
   (GET "/convert" {params :query-params server :server-name port :server-port} 
     (let [url (params "url")] 
       (str 
@@ -63,8 +73,10 @@
           read-request-body
           (grease-the-links server port (extract-host url)))))))
 
+; needed to gain access to query parameters in my-handler
 (def app
   (wrap-params my-handler))
 
+; entry point for running on heroku
 (defn -main [port]
   (jetty/run-jetty app {:port (Integer. port) :join? false}))
--- test/complico/acceptance_tests.clj
+++ test/complico/acceptance_tests.clj
@@ -6,7 +6,14 @@
       [ring.util.serve]))
 
 (def test-url-with-full-link "http://localhost:3000/test_full_link")
+
 (def test-url-with-relative-link "http://localhost:3000/test_relative_link")
+
+(def expected-base-html "<head><base href=\"http://localhost:3000/\"/></head>Test Page")
+
+(def expected-full-link "<a href=\"http://localhost:80/convert?url=http://somelink.com/\">")
+
+(def expected-relative-link "<a href=\"http://localhost:80/convert?url=http://localhost:3000/duff\">")
 
 (deftest convert
   ; start the server, headless
@@ -15,18 +22,21 @@
   (testing "should add host url as base element to request body"
     (let [response (app (request :get "/convert" {:url test-url-with-full-link} ))]
       (is (= (:status response) 200))
-      (is (= (.contains (:body response) 
-			"<head><base href=\"http://localhost:3000/\"/></head>Test Page") true))))
+      (is (.contains 
+              (:body response) 
+              expected-base-html))))
 
   (testing "should greasemonkey the full links on the page"
     (let [response (app (request :get "/convert" {:url test-url-with-full-link} ))] 
-      (is (= (.contains (:body response) 
-        "<a href=\"http://localhost:80/convert?url=http://somelink.com/\">") true))))
+      (is (.contains 
+            (:body response) 
+            expected-full-link))))
 
   (testing "should greasemonkey the relateive links on the page"
     (let [response (app (request :get "/convert" {:url test-url-with-relative-link} ))] 
-      (is (= (.contains (:body response) 
-        "<a href=\"http://localhost:80/convert?url=http://localhost:3000/duff\">") true))))
+      (is (.contains 
+            (:body response) 
+            expected-relative-link))))
 
   ;stop the ring server
   (stop-server))
