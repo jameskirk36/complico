b0debed7330bb720aa7baf79eeb0cd35dc1d6fa6
James Kirk <james.kirk84@gmail.com>
2014-08-15 16:38:10 +0100

    refactored tests

--- test/complico/core_test.clj
+++ test/complico/core_test.clj
@@ -8,17 +8,27 @@
 (def host "http://somelink.com/")
 
 (deftest build-link-replacements
-  (def ungreased-html 
-    "<link href=\"duff.css\"/>
-    <a href=\"http://anotherhost.com/duff\"/>
-    <a href=\"duff\"/>
-    <a href=\"/duff\"/>")
 
-  (def expected-link-replacements 
-    [["a href=\"http://anotherhost.com/duff\"" "a href=\"http://localhost:3000/convert?url=http://anotherhost.com/duff\""]
-      ["a href=\"duff\""                        "a href=\"http://localhost:3000/convert?url=http://somelink.com/duff\""]
-      ["a href=\"/duff\""                       "a href=\"http://localhost:3000/convert?url=http://somelink.com/duff\""]])
+  (testing "Dont grease the link elements"
+    (is (= (let [html "<link href=\"duff.css\"/>"]
+              (replace-all-in-string html 
+                 (build-link-replacement-map html server-name server-port host)))
+            "<link href=\"duff.css\"/>")))
 
-  (testing "Greasing the new links"
-    (is (= (build-link-replacement-map ungreased-html server-name server-port host) expected-link-replacements))))
+  (testing "Grease the anchor (full link)"
+    (is (= (let [html "<a href=\"http://anotherhost.com/duff\"/>"]
+              (replace-all-in-string html 
+                 (build-link-replacement-map html server-name server-port host)))
+            "<a href=\"http://localhost:3000/convert?url=http://anotherhost.com/duff\"/>")))
 
+  (testing "Grease the anchor (relative link no slash)"
+    (is (= (let [html "<a href=\"duff\"/>"]
+              (replace-all-in-string html 
+                 (build-link-replacement-map html server-name server-port host)))
+            "<a href=\"http://localhost:3000/convert?url=http://somelink.com/duff\"/>")))
+
+  (testing "Grease the anchor (relative link with slash)"
+    (is (= (let [html "<a href=\"/duff\"/>"]
+              (replace-all-in-string html 
+                 (build-link-replacement-map html server-name server-port host)))
+            "<a href=\"http://localhost:3000/convert?url=http://somelink.com/duff\"/>"))))
