0da205b0e6bab44b0ab53029f1afe69d16167621
James Kirk <james.kirk84@gmail.com>
2014-08-20 20:21:09 +0100

    split core tests up

--- test/complico/core_test.clj
+++ test/complico/core_test.clj
@@ -7,38 +7,42 @@
 (def server-port "3000")
 (def host "http://somelink.com/")
 
-(deftest build-link-replacements
-
+(deftest dont-grease-the-link-elements
   (testing "Dont grease the link elements"
     (is (= (let [html "<link href=\"duff.css\"/>"]
               (replace-all-in-string html 
                  (build-link-replacement-map html server-name server-port host)))
-            "<link href=\"duff.css\"/>")))
+            "<link href=\"duff.css\"/>"))))
 
+(deftest grease-the-anchor-links-full
   (testing "Grease the anchor (full link)"
     (is (= (let [html "<a href=\"http://anotherhost.com/duff\"/>"]
               (replace-all-in-string html 
                  (build-link-replacement-map html server-name server-port host)))
-            "<a href=\"http://localhost:3000/convert?url=http://anotherhost.com/duff\"/>")))
+            "<a href=\"http://localhost:3000/convert?url=http://anotherhost.com/duff\"/>"))))
 
+(deftest grease-the-anchor-links-relative-no-slash
   (testing "Grease the anchor (relative link no slash)"
     (is (= (let [html "<a href=\"duff\"/>"]
               (replace-all-in-string html 
                  (build-link-replacement-map html server-name server-port host)))
-            "<a href=\"http://localhost:3000/convert?url=http://somelink.com/duff\"/>")))
+            "<a href=\"http://localhost:3000/convert?url=http://somelink.com/duff\"/>"))))
 
+(deftest grease-the-anchor-links-relative-with-slash
   (testing "Grease the anchor (relative link with slash)"
     (is (= (let [html "<a href=\"/duff\"/>"]
               (replace-all-in-string html 
                  (build-link-replacement-map html server-name server-port host)))
-            "<a href=\"http://localhost:3000/convert?url=http://somelink.com/duff\"/>")))
+            "<a href=\"http://localhost:3000/convert?url=http://somelink.com/duff\"/>"))))
 
+(deftest grease-the-dirty-anchor
   (testing "Grease the dirty anchor"
     (is (= (let [html "<a class=\"class\" href=\"/duff\"/>"]
               (replace-all-in-string html 
                  (build-link-replacement-map html server-name server-port host)))
-            "<a class=\"class\" href=\"http://localhost:3000/convert?url=http://somelink.com/duff\"/>")))
+            "<a class=\"class\" href=\"http://localhost:3000/convert?url=http://somelink.com/duff\"/>"))))
 
+(deftest grease-the-area-links
   (testing "Grease the area element links"
     (is (= (let [html "<area href=\"http://anotherhost.com/duff\"/>"]
               (replace-all-in-string html 
