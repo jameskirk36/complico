ee7142252f478ee6916d0955772b1b30d42b373a
James Kirk <james.kirk84@gmail.com>
2014-09-01 16:33:32 +0100

    back to first principles

--- project.clj
+++ project.clj
@@ -6,10 +6,9 @@
   :dependencies [
                   [org.clojure/clojure "1.5.1"]
                   [ring/ring-core "1.1.8"]
+                  [ring/ring-jetty-adapter "1.1.8"]
                   [compojure "1.1.5"]
-						[ring-mock "0.1.5"]
-						[clj-http "0.9.2"]
-						[ring-serve "0.1.2"]]
+						[clj-webdriver "0.6.1"]]
   
   :uberjar-name "complico.jar"
   :plugins  [[lein-ring "0.8.3"]]
--- src/complico/core.clj
+++ src/complico/core.clj
@@ -1,137 +1,22 @@
 (ns complico.core
   (use compojure.core)
   (use compojure.route)
-  (use ring.middleware.params)
-  (:require [clj-http.client :as client])
-  (:require [clojure.string :as string])
   (:require [ring.adapter.jetty :as jetty]))
 
-; extract just the host from a URI
-(defn extract-host [url]
-  (str  
-    (string/join "/" 
-      (take 3 
-        (string/split url #"/"))) "/")) 
-
-; create base html
-(defn create-base-html [url]
-  (str "<head><base href=\"" url \""/></head>"))
-
-; make a http request to url and download the body as string
-(defn read-request-body [url] 
-  (:body (client/get url)))
-
-; find a sequence of anchor/area links in a given body
-(defn find-links [body]
-  (re-seq #"<(a.*?)href=\"(.+?)\"" body))
-
-; create the prefix to add to all links
-(defn create-grease [server port]
-  (str "http://" server ":" port "/convert?url="))
-
-; apply grease to different kinds of links
-(defn grease-the-link [element link grease host]
-  (cond
-    (.startsWith link "http") (str "<" element "href=\"" grease link "\"")
-    (.startsWith link "/") (str "<" element "href=\"" grease (apply str (drop-last 1 host)) link "\"") 
-    :else (str "<" element "href=\"" grease host link "\""))) 
-
-; replace all the occurances of a find-replace map in a string
-(defn replace-all-in-string [string replacement-map]
-  (str
-    (reduce 
-      #(apply string/replace %1 %2)
-      string
-      replacement-map
-     )))
-
-
-; build a map of links and their greased replacements
-(defn build-greased-link-map [links grease host]
-  (map 
-    #(vector 
-       (first %)
-       (grease-the-link 
-         (second %)
-         (last %)
-         grease
-         host))
-    links))
-
-; find all links and build a map of links and their greased replacement
-(defn build-link-replacement-map [body server port host]
-  (let [links (find-links body)
-        grease (create-grease server port)]
-        (build-greased-link-map links grease host)))
-
-(defn find-forms [body] 
-  (re-seq #"<form\s(.*?)action=\"(.*?)\"(.*?)>" body))
-
-(defn grease-the-form [attributes-before action attributes-after grease]
-  (str "<form " attributes-before "action=\"" grease "\"" attributes-after ">\n" "<input type=\"hidden\" name=\"url\" value=\"" action "?test=test\"/>" ))
-
-(defn grease-the-forms [forms host]
-  (map 
-    #(vector 
-      (nth % 0)
-      (grease-the-form 
-        (nth % 1)
-        (nth % 2)
-        (nth % 3)
-        host))
-    forms))
-
-(defn build-form-replacement-map [body server port]
-  (grease-the-forms (find-forms body) (str "http://" server ":" port "/convert")))
-
-(defn find-prices [body]
-  (re-seq #"£(\d+\.?\d*)" body))
-
-
-(defn convert-price [price]
-  "£XXX")
-
-(defn convert-prices [prices]
-  (map
-    #(vector
-      (first %)
-      (convert-price (last %)))
-    prices))
-  
-
-(defn build-price-replacement-map [body]
-  (let [prices (find-prices body)]
-    (convert-prices prices)))
+(def link-to-second-page 
+  "<a id=\"second_page\" href=\"http://localhost:3000/convert?url=http%3A%2F%2Flocalhost%3A3000%2Fsecond_test_page\">Second page link</a>")
 
 (defroutes my-handler
-  (GET "/" [] "Welcome")
-  ; routes for testing only
-  (GET "/test_full_link" [] "Test Page<a href=\"http://somelink.com/\">")
-  (GET "/test_relative_link" [] "Test Page<a href=\"/duff\">")
-  (GET "/test_prices" [] "Test Page £2 £3.00 £NOTTHIS")
-  (GET "/test_form" [] 
-"<form class=\"\" action=\"http://anotherhost.com/duff\">
-<input name=\"name\" value=\"value\"/>
-</form>")
-  ; entry point for complicos complicated prices!
-  (GET "/convert" {params :query-params server :server-name port :server-port} 
-    (let [url (params "url")
-          host (extract-host url)] 
-      (str 
-        (create-base-html host)
-        (let [body (read-request-body url)
-              host (extract-host url)]
-          (let [replacement-link-map (build-link-replacement-map body server port host)
-                replacement-form-map (build-form-replacement-map body server port)
-                replacement-prices-map (build-price-replacement-map body)]
-
-            (replace-all-in-string body (into [] (concat replacement-link-map replacement-prices-map replacement-form-map)))))))))
+  (GET "/first_test_page" [] 
+    link-to-second-page)
+  (GET "/convert" []
+    "<title>Second test page</title>"))
 
 ; needed to gain access to query parameters in my-handler
-(def app
-  (wrap-params my-handler))
+(def app my-handler)
 
 ; entry point for running on heroku
 (defn -main [port]
   (jetty/run-jetty app {:port (Integer. port) :join? false}))
+
 
