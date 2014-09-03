3480cfc795d8c30cacb65f5835447c2deb489e74
James Kirk <james.kirk84@gmail.com>
2014-09-03 07:40:06 +0100

    js for replaceprices and test

--- src/complico/core.clj
+++ src/complico/core.clj
@@ -9,23 +9,31 @@
 (defn request-url-page [url] 
   (:body (client/get url)))
 
-(defn create-link-greasing-js [server port]
+(defn create-js [server port]
   (str "<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script>
   <script type=\"text/javascript\">
   $(function(){
-  
-    var foundin = $('a').each(function(index){
-    var link = $(this).attr('href');
-    var newLink = 'http://" server ":" port "/convert?url=' + encodeURIComponent(link);
-    $(this).attr('href', newLink); 
-  });
-  });
+    var greaseLinks = function(){ 
+      var foundin = $('a').each(function(index){
+        var link = $(this).attr('href');
+        var newLink = 'http://" server ":" port "/convert?url=' + encodeURIComponent(link);
+        $(this).attr('href', newLink); 
+      });
+    };
+    var replacePrices = function(){
+      var found = $('div').each(function(index){
+        $(this).text('£XXX');
+      });
+    };
+    greaseLinks();
+    replacePrices();
+   }); 
   </script>"))
 
 (def start-page-html 
   "<a id=\"test_page_with_prices\" href=\"http://localhost:3000/test_page_with_prices\">Show prices</a>")
 (def price-page-html
- "<div id=\"price\">£XXX</div>")
+ "<div id=\"price\">£3</div>")
 
 (defroutes my-handler
   (GET "/test_start_page" [] 
@@ -33,7 +41,7 @@
   (GET "/test_page_with_prices" []
     price-page-html)
   (GET "/convert" {params :query-params server :server-name port :server-port}
-    (str (request-url-page (params "url")) (create-link-greasing-js server port))))
+    (str (request-url-page (params "url")) (create-js server port))))
 
 ; needed to gain access to query parameters in my-handler
 (def app 
