3cc6e067e0604470319f5f018e6d831e30e01c27
James Kirk <james.kirk84@gmail.com>
2015-02-22 07:52:25 +0000

    refactoring dom helper

--- src-cljs/complico/dom_helper.cljs
+++ src-cljs/complico/dom_helper.cljs
@@ -1,25 +1,28 @@
 (ns complico.dom-helper)
 
 (defn is-text-node [node] 
-  (= (.-nodeType node) 3))
+  (-> node
+    (.-nodeType)
+    (= 3)))
 
 (defn get-child-nodes [node]
   (.-childNodes node))
 
 (defn get-text-node [node]
-  (first (filter #(is-text-node %) (get-child-nodes node))))
+  (->> node 
+    (get-child-nodes)
+    (filter is-text-node)
+    (first)))
 
 (defn get-text-from-node [node]
-  (let [text-node (get-text-node node)]
-    (if (= text-node nil)
-      nil
-      (.-nodeValue text-node))))
+  (if-let [text-node (get-text-node node)]
+    (.-nodeValue text-node)))
 
 (defn set-text-on-node [node text]
-  (set! (.-nodeValue node) text))
+  (-> node
+    (.-nodeValue)
+    (set! text)))
 
 (defn replace-text-on-node! [elem text]
   (let [node (get-text-node elem)]
-    (set-text-on-node node text))
-  elem)
-
+    (set-text-on-node node text)))
